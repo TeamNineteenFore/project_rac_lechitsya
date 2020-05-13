8d0cab805274c2353aebae29727e20cb9af7f9d6
Shystriyhlopchik <Shystriyhlopchik@yandex.ru>
2020-05-13 23:57:17 +0300

    Компонент Cover поправлен

--- components/Cover.vue
+++ components/Cover.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="cover main__cover">
-    <video class="cover__video">
-      <source
-        src="video.mp4"
-        type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
-      />
-      <source src="video.ogg" type='video/ogg; codecs="theora, vorbis"' />
-      <source src="video.webm" type='video/webm; codecs="vp8, vorbis"' />
-    </video>
-    <h1 class="cover__title">#РАКЛЕЧИТСЯ</h1>
+    <cover-video />
+    <hashtag class="cover__title">#РАКЛЕЧИТСЯ</hashtag>
     <a href="#" class="cover__button"></a>
   </div>
 </template>
 
 <script>
-export default {};
+import Hashtag from '@/components/ui/Hashtag';
+import CoverVideo from '@/components/ui/CoverVideo';
+
+export default {
+  components: {
+    hashtag: Hashtag,
+    'cover-video': CoverVideo,
+  },
+};
 </script>
 
 <style scoped>
 .cover {
   position: relative;
-}
-
-.cover__video {
-  background-color: #613a93;
-  position: relative;
-  width: 100%;
-  height: 100vh;
 }
 
 .cover__title {
---
+++ components/ui/CoverVideo.vue
@@ -0,0 +1,20 @@
+<template>
+  <video class="cover__video">
+    <source src="video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
+    <source src="video.ogg" type='video/ogg; codecs="theora, vorbis"' />
+    <source src="video.webm" type='video/webm; codecs="vp8, vorbis"' />
+  </video>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.cover__video {
+  background-color: #613a93;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+</style>
